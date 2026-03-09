5ca8ce9e6b699bdd826b88793d2458b608bc908f
kellianneng <168501224+kellianneng@users.noreply.github.com>
2026-03-09 17:55:35 +0800

    Update merge_model_dataset.py with the correct train and test set

--- data extraction code/merge_model_dataset.py
+++ data extraction code/merge_model_dataset.py
@@ -73,15 +73,22 @@ for lag in lags:
     lagged_block.columns = [f"{c}_lag{lag}" for c in feature_cols]
     full_df = pd.concat([full_df, lagged_block], axis=1)
 
-# After lag, missing values for 5 jan, 6 jan, 7 jan. Data can only start from 8 jan 
-full_df = full_df.dropna(subset=[f"{feature_cols[0]}_lag3"]).reset_index(drop=True)
+# Feature columns now include Xt and lagged versions
+lag_feature_cols = [f"{c}_lag{lag}" for c in feature_cols for lag in lags]
+feature_cols = feature_cols + lag_feature_cols
+
+# Drop first 3 rows (or any rows where lagged predictors are missing)
+full_df = full_df.dropna(subset=lag_feature_cols).reset_index(drop=True)
 
 # Drop raw RV since not relevant 
 full_df = full_df.drop(columns = ["RV"])
 print(full_df.head(5))
+
+# Saving the full dataset
 full_df.to_csv("../data/full_df.csv", index=False)
 
-# Part 3: Create train test sets 
+# Part 3: Create train test datasets 
+
 # Sort data again just in case
 full_df = full_df.sort_values("date").reset_index(drop=True)
 
@@ -96,66 +103,13 @@ cut = int(len(full_df) * 0.7)
 train_df = full_df.iloc[:cut].copy() # correct logic but still contains present day values which wouldnt be observed on training/tetsing day so use X_train
 test_df  = full_df.iloc[cut:].copy() # same as above
 
-# Part 4: Split into X/y (for various prediction horizons: 1-day, 3-days, 5-days, and 7-days ahead))
-# choose whether to include present-day features too
-present_feature_cols = feature_cols
-model_feature_cols = present_feature_cols + lagged_feature_cols
-# if you want only lagged, use: model_feature_cols = lagged_feature_cols
-X_train = train_df[model_feature_cols].copy()
-X_test  = test_df[model_feature_cols].copy()
 
-y_train_h1 = train_df[["date", "y_h1"]].copy()
-y_test_h1  = test_df[["date", "y_h1"]].copy()
-y_train_h3 = train_df[["date", "y_h3"]].copy()
-y_test_h3  = test_df[["date", "y_h3"]].copy()
-y_train_h5 = train_df[["date", "y_h5"]].copy()
-y_test_h5  = test_df[["date", "y_h5"]].copy()
-y_train_h7 = train_df[["date", "y_h7"]].copy()
-y_test_h7  = test_df[["date", "y_h7"]].copy()
-
-# create train and test csv (not standardised)
-train_df.to_csv("../data/train_dataset.csv", index=False)
-test_df.to_csv("../data/test_dataset.csv", index=False)
-
-# Part 5: Standardize features (fit on train only)
-scaler = StandardScaler()
-
-X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train),
-    columns=X_train.columns,
-    index=X_train.index)
-
-X_test_scaled = pd.DataFrame(scaler.transform(X_test),
-    columns=X_test.columns,
-    index=X_test.index)
-
-# optional: keep date for alignment
-X_train_scaled_with_date = pd.concat([train_df[["date"]].reset_index(drop=True), X_train_scaled.reset_index(drop=True)], axis=1)
-X_test_scaled_with_date = pd.concat([test_df[["date"]].reset_index(drop=True), X_test_scaled.reset_index(drop=True)], axis=1)
-
-# Part 6: Save the remaining datasets for modelling 
-X_train_scaled.to_csv("../data/X_train_scaled.csv", index=False)
-X_test_scaled.to_csv("../data/X_test_scaled.csv", index=False)
-
-X_train_scaled_with_date.to_csv("../data/X_train_scaled_with_date.csv", index=False)
-X_test_scaled_with_date.to_csv("../data/X_test_scaled_with_date.csv", index=False)
-# print("Train date range:", X_train_scaled_with_date["date"].min(), "to", train_df["date"].max(), "rows:", len(train_df))
+print("Train date range:", train_df["date"].min(), "to", train_df["date"].max(), "rows:", len(train_df))
 # Train date range: 2021-01-08 00:00:00 to 2024-06-28 00:00:00 rows: 1268
-# print("Test  date range:", X_test_scaled_with_date["date"].min(),  "to", test_df["date"].max(),  "rows:", len(test_df))
+
+print("Test  date range:", test_df["date"].min(),  "to", test_df["date"].max(),  "rows:", len(test_df))
 # Test  date range: 2024-06-29 00:00:00 to 2025-12-24 00:00:00 rows: 544
 
-## CAN UNCOMMNENT THE FOLLOWING
-# y_train_h1.to_csv("../data/y_train_h1.csv", index=False)
-# y_test_h1.to_csv("../data/y_test_h1.csv", index=False)
-
-# y_train_h3.to_csv("../data/y_train_h3.csv", index=False)
-# y_test_h3.to_csv("../data/y_test_h3.csv", index=False)
-
-# y_train_h5.to_csv("../data/y_train_h5.csv", index=False)
-# y_test_h5.to_csv("../data/y_test_h5.csv", index=False)
-
-# y_train_h7.to_csv("../data/y_train_h7.csv", index=False)
-# y_test_h7.to_csv("../data/y_test_h7.csv", index=False)
-
-
-
-
+# create train and test csv
+#train_df.to_csv("../data/train_dataset.csv", index=False)
+#test_df.to_csv("../data/test_dataset.csv", index=False)
