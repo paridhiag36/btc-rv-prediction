c0222e388d2a85d556778b43a83cfb53e155358f
paridhiagarwal <agarwalparidhi10@gmail.com>
2026-03-16 15:50:39 +0800

    Plots for h = 1, 3, 5, 7 for XGBoost pred uploaded

--- models/xgboost/xgboost_plots.py
+++ models/xgboost/xgboost_plots.py
@@ -5,13 +5,15 @@ import os
 
 
 # Load rolling OOS predictions
-preds = pd.read_csv("../forecast evaluations/xgboost/xgb_rolling_oos_predictions.csv")
+preds = pd.read_csv("../../forecast evaluations/xgb_outputs/xgb_rolling_oos_predictions.csv")
 preds["date"] = pd.to_datetime(preds["date"])
-preds = preds.sort_values(["h", "date"])
+preds = preds.sort_values(["h", "date"]) # Ensure sorting by h and date
+# print(preds.head())
+# print (preds.tail())
 
 print("Unique h:", sorted(preds["h"].unique()))
 
-os.makedirs("../figs", exist_ok=True)
+os.makedirs("../../figs", exist_ok=True)
 
 for h in sorted(preds["h"].unique()):
     df_h = preds[preds["h"] == h].dropna(subset=["y_true"])
@@ -24,7 +26,7 @@ for h in sorted(preds["h"].unique()):
     plt.ylabel("Target (log RV)")
     plt.legend()
     plt.tight_layout()
-    plt.savefig(f"../figs/xgb_actual_vs_pred_h{h}.png", dpi=200)
+    plt.savefig(f"../../figs/xgb_actual_vs_pred_h{h}.png", dpi=200)
     plt.close()
 
-print("Saved plots to ../figs/")
+print("Saved plots to ../../figs/")
